ec6f80539baca989c7cbd317a8d8571980b73f05
Thiago <swfsql@gmail.com>
2019-05-01 20:37:03 -0300

    benchmark updated

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,35 +1,30 @@
 #[macro_use]
 extern crate criterion;
 
+extern crate permutate;
+
+// use permutate::{Permutator, PermutatorWrapper as _};
+use permutate::Permutator;
 use criterion::Criterion;
 use criterion::black_box;
 
-
-fn blabla() {
-    let a = ["A0", "B1", "C2", "D3", "E4", "F5", "G6", "H8", "I8", "J9"];
-
-    let b = [
-        &a[..], // 0
-        &a[..], // 1
-        &a[..], // 2
-        &a[..], // 3
-        &a[..], // 4
-        &a[..], // 5
-        &a[..], // 6
-        &a[..], // 7
-    ];
+fn get_input<'a>() -> [&'a [&'a str]; 4] {
+    [
+        &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"][..], // 10
+        &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"][..], // 100
+        &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"][..], // 1k
+        &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"][..], // 10k
+    ]
 }
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
-    }
+// Check to see if exactly 10,000 permutations were collected.
+fn test_10k_permutations() {
+    let input: Vec<&[&str]> = get_input().to_vec();
+    assert_eq!(10_000, Permutator::new(&input).count())
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+    c.bench_function("perm 10k", |b| b.iter(|| test_10k_permutations()));
 }
 
 criterion_group!(benches, criterion_benchmark);
